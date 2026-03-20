148a78bd3b6c0e4f627f4247e9fdb55cc30dc31d
ashleywong558 <ashleywong558@gmail.com>
2026-03-20 16:48:14 -0700

    Refactor sensor initialization and data processing
    Updated sensor initialization and data handling.

--- working_cam+sensor/rasp_sensor.py
+++ working_cam+sensor/rasp_sensor.py
@@ -1,20 +1,14 @@
 import time
-import numpy as np
 from vl53l5cx_sensor import VL53L5CXSensor
 
-sensor = VL53L5CXSensor()
+sensor = VL53L5CXSensor(use_serial=False)
 
-print("Initializing sensor...")
-sensor.start_ranging()
+print("Sensor running...")
 
 while True:
-    if sensor.check_data_ready():
-        data = sensor.get_ranging_data()
+    data = sensor.get_ranging_data()
 
-        # Convert to 8x8 grid
-        distances = np.array(data).reshape((8, 8))
-
-        print("\nFrame:")
-        print(distances)
+    if data is not None:
+        sensor.print_distance_array(data)
 
     time.sleep(0.05)
